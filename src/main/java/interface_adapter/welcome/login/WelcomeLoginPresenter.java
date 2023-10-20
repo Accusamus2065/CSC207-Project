0a56e83d176c359676fe7416c7f3bab1dc52e159
tamlong5 <dexter.tam@mail.utoronto.ca>
2023-10-20 17:01:06 -0400

    Initialised WelcomeLoginPresenter and WelcomeSignupPresenter

---
+++ src/main/java/interface_adapter/welcome/login/WelcomeLoginPresenter.java
@@ -0,0 +1,11 @@
+package interface_adapter.welcome.login;
+
+import use_case.welcome.login.WelcomeLoginOutputBoundary;
+
+public class WelcomeLoginPresenter implements WelcomeLoginOutputBoundary {
+
+    @Override
+    public void swapViews() {
+        // TODO: complete me; depends on SignupViewModel
+    }
+}
---
+++ src/main/java/interface_adapter/welcome/signup/WelcomeSignupPresenter.java
@@ -0,0 +1,11 @@
+package interface_adapter.welcome.signup;
+
+import use_case.welcome.signup.WelcomeSignupOutputBoundary;
+
+public class WelcomeSignupPresenter implements WelcomeSignupOutputBoundary {
+
+    @Override
+    public void swapViews() {
+        // TODO: complete me; depends on SignupViewModel
+    }
+}
