fe1b31f190913be37973f4805bf2ef6b413e6b1a
tamlong5 <dexter.tam@mail.utoronto.ca>
2023-10-20 16:29:08 -0400

    Added instance attributes for WelcomeViewModel

--- src/main/java/interface_adapter/welcome/WelcomeViewModel.java
+++ src/main/java/interface_adapter/welcome/WelcomeViewModel.java
@@ -1,4 +1,7 @@
 package interface_adapter.welcome;
 
 public class WelcomeViewModel {
+    public static final String TITLE_LABEL = "Welcome!";
+    public static final String SIGNUP_BUTTON_LABEL = "Sign Up";
+    public static final String SIGNIN_BUTTON_LABEL = "Sign In";
 }
