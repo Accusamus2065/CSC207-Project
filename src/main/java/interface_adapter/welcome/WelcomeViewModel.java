cce0fa1cea8a4bce7d39a859ace65c290239363b
tamlong5 <dexter.tam@mail.utoronto.ca>
2023-10-20 17:14:26 -0400

    Added inherited methods to WelcomeViewModel

--- src/main/java/interface_adapter/welcome/WelcomeViewModel.java
+++ src/main/java/interface_adapter/welcome/WelcomeViewModel.java
@@ -1,7 +1,32 @@
 package interface_adapter.welcome;
 
-public class WelcomeViewModel {
+import interface_adapter.ViewModel;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class WelcomeViewModel extends ViewModel {
     public static final String TITLE_LABEL = "Welcome!";
     public static final String SIGNUP_BUTTON_LABEL = "Sign Up";
     public static final String LOGIN_BUTTON_LABEL = "Sign In";
+    private WelcomeState state;
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    public WelcomeViewModel(String viewName) {
+        super("Welcome");
+    }
+
+    public void setState(WelcomeState state) {
+        this.state = state;
+    }
+
+    // The methods below should never be called since WelcomeView should never be modified
+    @Override
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
 }
