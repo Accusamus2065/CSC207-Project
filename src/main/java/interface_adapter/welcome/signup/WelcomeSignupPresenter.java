f4395f1972e183ed07811df1c922cb62cc203c38
tamlong5 <dexter.tam@mail.utoronto.ca>
2023-10-20 17:38:59 -0400

    Added TODO to WelcomeSignupPresenter

--- src/main/java/interface_adapter/welcome/signup/WelcomeSignupPresenter.java
+++ src/main/java/interface_adapter/welcome/signup/WelcomeSignupPresenter.java
@@ -3,7 +3,6 @@ package interface_adapter.welcome.signup;
 import use_case.welcome.signup.WelcomeSignupOutputBoundary;
 
 public class WelcomeSignupPresenter implements WelcomeSignupOutputBoundary {
-
     @Override
     public void swapViews() {
         // TODO: complete me; depends on SignupViewModel
