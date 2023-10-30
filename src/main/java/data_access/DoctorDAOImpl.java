6e114525b9b57c5758314dba2dbb1dff54886c2f
jihyuky <jihyuky@gmail.com>
2023-10-30 16:39:27 -0400

    Implemented DoctorDAO. Removed save() from SignupUserDataAccessInterface because it caused error. Might need to fix later

--- src/main/java/data_access/DoctorDAOImpl.java
+++ src/main/java/data_access/DoctorDAOImpl.java
@@ -2,10 +2,7 @@ package data_access;
 
 import com.mongodb.client.*;
 import entity.mongo.MongoFactory;
-import entity.people.Doctor;
-import entity.people.DoctorUserFactory;
-import entity.people.PatientUserFactory;
-import entity.people.User;
+import entity.people.*;
 import org.bson.Document;
 import use_case.signup.SignupUserDataAccessInterface;
 
@@ -14,7 +11,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class DoctorDAOImpl implements SignupUserDataAccessInterface {
-    private final Map<String, User> accounts = new HashMap<>();
+    private final Map<String, IDoctor> accounts = new HashMap<>();
     private static final MongoClient mongoClient = MongoFactory.setUpMongoClient();
 
     public DoctorDAOImpl(DoctorUserFactory doctorUserFactory) {
@@ -30,12 +27,11 @@ public class DoctorDAOImpl implements SignupUserDataAccessInterface {
         for (Document document : findIterable) {
             //public User create(String name, String password, LocalDateTime ldt, char sex, String gender, float height, float weight, String bloodType)
             accounts.put(document.getString("username"), doctorUserFactory.create(document.getString("name"),
-                    document.getString("password"), (LocalDateTime) document.get("ldt"),
-                    document.getString("degree"), );
+                    document.getString("password"), document.getString("degree"), document.getString("specialty")));
         }
 
         // Printing the data stored in the Map object
-        for (Map.Entry<String, User> entry : accounts.entrySet()) {
+        for (Map.Entry<String, IDoctor> entry : accounts.entrySet()) {
             System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
         }
 
@@ -48,10 +44,9 @@ public class DoctorDAOImpl implements SignupUserDataAccessInterface {
         return accounts.containsKey(identifier);
     }
 
-    @Override
-    public void save(User user) {
-        accounts.put(user.getUsername(), user);
-        saveRemote(user);
+    public void save(IDoctor doctor) {
+        accounts.put(doctor.getUsername(), doctor);
+        saveRemote(doctor);
     }
 
     private void saveRemote(User user) {
