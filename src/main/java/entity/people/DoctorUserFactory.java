c14ceb515c4ff48b9fd9f7fc84fce61216fd02bc
jihyuky <jihyuky@gmail.com>
2023-10-30 16:18:25 -0400

    Implemented Doctor Inteface

--- src/main/java/entity/people/DoctorUserFactory.java
+++ src/main/java/entity/people/DoctorUserFactory.java
@@ -5,12 +5,12 @@ import java.time.LocalDateTime;
 public class DoctorUserFactory implements UserFactory {
 
     @Override
-    public User create(String name, String password, LocalDateTime ldt) {
+    public User create(String name, String password) {
         return new Doctor(name, password);
     }
 
-    public User create(String name, String password, LocalDateTime ldt, String degree, String specialty) {
-        Doctor doc = (Doctor) create(name, password, ldt);
+    public Doctor create(String name, String password, String degree, String specialty) {
+        Doctor doc = (Doctor) create(name, password);
         doc.setDegree(degree);
         doc.setSpecialty(specialty);
         return doc;
--- src/main/java/entity/people/IDoctor.java
+++ src/main/java/entity/people/IDoctor.java
@@ -2,20 +2,20 @@ package entity.people;
 
 import java.time.LocalDateTime;
 
-public interface IDoctor {
+public interface IDoctor extends User{
     String getUsername();
 
     String getPassword();
     String getSpecialty();
     String getDegree();
 
-    public void setUsername(String username);
+    void setUsername(String username);
 
-    public void setPassword(String password);
+    void setPassword(String password);
 
-    public void setDegree(String degree);
+    void setDegree(String degree);
 
-    public void setSpecialty(String specialty);
-    }
+    void setSpecialty(String specialty);
+
 
 }
