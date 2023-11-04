74b810f0476af3e04138a267935ce7bc7e96e7c2
Yibin Cui <110690068+Accusamus2065@users.noreply.github.com>
2023-11-03 21:18:10 -0400

    add save(User user) method for ConvoDAO

--- src/main/java/use_case/signup/SignupUserDataAccessInterface.java
+++ src/main/java/use_case/signup/SignupUserDataAccessInterface.java
@@ -3,9 +3,11 @@ package use_case.signup;
 
 import entity.people.IDoctor;
 import entity.people.IPatient;
+import entity.people.User;
 
 public interface SignupUserDataAccessInterface {
     void save(IPatient user);
     void save(IDoctor user);
+    void save(User user);
     boolean existsByName(boolean isDoctor, String identifier);
 }
