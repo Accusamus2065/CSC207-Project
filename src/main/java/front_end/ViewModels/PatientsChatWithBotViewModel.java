14a17ce2c97ea1ce46fbf1108c223e0124c779c0
Riyad Valiyev <128013175+riyadvaliyev@users.noreply.github.com>
2023-11-13 16:33:12 -0500

    NOTE FOR THE TA: Over the past 5 weeks, I have created these 20 files with over thousands of lines of code. This contains all the front-end for our project. I have been working on all of these in a different project repo directory because the rest of the team works on back-end and my development of front-end needed to be independent of their work (for testing/launching frames to see the designs that I make along the way). More clearly, I needed to make launch them quite often as I worked, so I could make quick changes and this couldn't have been the case while the others were working on  the back-end. I am now committing all these lines of code in one push. But you please evaluate my work by the number of lines of code contributed rather than the number of commits (which is only 1). So, I am leaving this note.

---
+++ src/main/java/front_end/ViewModels/PatientsChatWithBotViewModel.java
@@ -0,0 +1,21 @@
+package front_end.ViewModels;
+
+import java.awt.*;
+
+public class PatientsChatWithBotViewModel {
+    public static final String TITLE_LABEL = "ChatBox with Bot";
+    public static final int FRAME_WIDTH_SIZE = 800;
+    public static final int FRAME_HEIGHT_SIZE = 500;
+    public static final Dimension PANEL_DIMENSION = new Dimension(800, 500);
+    public static final String LOGOUT_BUTTON_LABEL = "Log Out";
+    public static final String MODIFY_BUTTON_LABEL = "Modify Profile";
+    public static final Font BUTTON_FONT = new Font("Arial", Font.PLAIN, 15);
+    public static final Dimension BUTTON_DIMENSION = new Dimension(110, 45);
+    public static final String MAIN_LABEL = "DialogFlow";
+    public static final Font MAIN_LABEL_FONT = new Font("Sans-serif", Font.BOLD, 40);
+    public static final Dimension CHAT_AREA_DIMENSION = new Dimension(500, 250);
+    public static final Font MESSAGE_FIELD_FONT = new Font("Sans-serif", Font.PLAIN, 20);
+    public static final String SEND_BUTTON_LABEL = "Send";
+    public static final Font SEND_BUTTON_FONT = new Font("Sans-serif", Font.BOLD, 20);
+    public static final Dimension SEND_BUTTON_DIMENSION = new Dimension(100, 47);
+}
