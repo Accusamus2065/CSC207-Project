79ad60165c5c6a8cb9f02d0623611b9e4b00b1e4
jihyuky <jihyuky@gmail.com>
2023-11-13 16:14:15 -0500

    Implemented successDoctorTest and successPatientTest

--- src/test/java/use_case/signup/SignupInteractorTest.java
+++ src/test/java/use_case/signup/SignupInteractorTest.java
@@ -1,6 +1,52 @@
 package use_case.signup;
 
+import data_access.InMemoryUserDataAccessObject;
+import entity.people.DoctorUserFactory;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class SignupInteractorTest {
-    void successTest(){
+    @Test
+    public void successDoctorTest(){
+        SignupInputData inputData = new SignupInputData(
+                "doctor1", "password", "password", true);
+        SignupUserDataAccessInterface userRepository = new InMemoryUserDataAccessObject();
+
+        SignupOutputBoundary successPresenter = new SignupOutputBoundary(){
+            public void prepareSuccessView(SignupOutputData user){
+                assertEquals("doctor1", user.getUsername());
+                assertTrue(userRepository.existsByName(true, "tester"));
+            }
+
+            public void prepareFailView(String error){
+                fail("User case failure is not expected");
+            }
+
+        };
+        SignupInputBoundary interactor = new SignupInteractor(userRepository, successPresenter, new DoctorUserFactory());
+        interactor.execute(inputData);
+    }
+
+    @Test
+    public void successPatientTest(){
+        SignupInputData inputData = new SignupInputData(
+                "patient1", "password", "password", false);
+        SignupUserDataAccessInterface userRepository = new InMemoryUserDataAccessObject();
+
+        SignupOutputBoundary successPresenter = new SignupOutputBoundary(){
+            public void prepareSuccessView(SignupOutputData user){
+                assertEquals("patient1", user.getUsername());
+                assertTrue(userRepository.existsByName(false, "tester"));
+            }
+
+            public void prepareFailView(String error){
+                fail("User case failure is not expected");
+            }
+
+        };
+        SignupInputBoundary interactor = new SignupInteractor(userRepository, successPresenter, new DoctorUserFactory());
+        interactor.execute(inputData);
     }
 }
