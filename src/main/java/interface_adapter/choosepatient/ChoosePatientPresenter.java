da6deb30701703485ad252d6f86ce833d45a54de
jihyuky <jihyuky@gmail.com>
2023-11-17 14:35:13 -0500

    Worked on interactor and presenter

--- src/main/java/interface_adapter/choosepatient/ChoosePatientPresenter.java
+++ src/main/java/interface_adapter/choosepatient/ChoosePatientPresenter.java
@@ -4,6 +4,7 @@ package interface_adapter.choosepatient;//package interface_adapter.login;
 
 import interface_adapter.ViewManagerModel;
 
+import interface_adapter.chat.ConversationState;
 import interface_adapter.chat.ConversationViewModel;
 import use_case.login.LoginOutputBoundary;
 import use_case.login.LoginOutputData;
@@ -13,37 +14,39 @@ import java.util.List;
 
 public class ChoosePatientPresenter implements LoginOutputBoundary {
 
-    private final LoginViewModel loginViewModel;
-    private final LoggedInViewModel loggedInViewModel;
+    private final ChoosePatientViewModel choosePatientViewModel;
+    private final ConversationViewModel conversationViewModel;
     private ViewManagerModel viewManagerModel;
 
-    public ChoosePatientPresenter(ViewManagerModel viewManagerModel,
-                           ConversationViewModel conversationViewModel,
-                          
-                          ChoosePatientViewModel choosePatientViewModel) {
+    public ChoosePatientPresenter(ChoosePatientViewModel choosePatientViewModel,
+                                  ConversationViewModel conversationViewModel,
+                                  ViewManagerModel viewManagerModel) {
+        this.choosePatientViewModel = choosePatientViewModel;
+        this.conversationViewModel = conversationViewModel;
         this.viewManagerModel = viewManagerModel;
         this.loggedInViewModel = loggedInViewModel;
         this.loginViewModel = loginViewModel;
     }
 
+
     @Override
     public void prepareSuccessView(LoginOutputData response) {
         // On success, switch to the logged in view.
 
-        LoggedInState loggedInState = loggedInViewModel.getState();
-        loggedInState.setUsername(response.getUsername());
-        this.loggedInViewModel.setState(loggedInState);
-        this.loggedInViewModel.firePropertyChanged();
+        ConversationState conversationState = conversationViewModel.getState();
+        conversationState.setUser(response.getUsername()); // TODO conversation state needs to be fixed, but not my file
+        this.conversationViewModel.setState(conversationState);
+        this.conversationViewModel.firePropertyChanged();
 
-        this.viewManagerModel.setActiveView(loggedInViewModel.getViewName());
+        this.viewManagerModel.setActiveView(conversationViewModel.getViewName());
         this.viewManagerModel.firePropertyChanged();
     }
 
     @Override
     public void prepareFailView(String error) {
-        LoginState loginState = loginViewModel.getState();
-        loginState.setUsernameError(error);
-        loginViewModel.firePropertyChanged();
+        ConversationState conversationState = conversationViewModel.getState();
+        conversationState.setError(error); // TODO need to implement this too.
+        conversationViewModel.firePropertyChanged();
     }
 
 
