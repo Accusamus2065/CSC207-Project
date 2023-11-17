a3001d6998cbc69d43b9a307c33ad7e7eac7504f
jihyuky <jihyuky@gmail.com>
2023-11-17 15:13:03 -0500

    loginpresenter fixed

--- src/main/java/interface_adapter/login/LoginPresenter.java
+++ src/main/java/interface_adapter/login/LoginPresenter.java
@@ -1,46 +1,46 @@
-//package interface_adapter.login;
-//
-//import interface_adapter.logged_in.LoggedInState;
-//import interface_adapter.logged_in.LoggedInViewModel;
-//import interface_adapter.ViewManagerModel;
-//import interface_adapter.signup.SignupState;
-//import use_case.login.LoginOutputBoundary;
-//import use_case.login.LoginOutputData;
-//import use_case.signup.SignupOutputBoundary;
-//import use_case.signup.SignupOutputData;
-//
-//public class LoginPresenter implements LoginOutputBoundary {
-//
-//    private final LoginViewModel loginViewModel;
-//    private final LoggedInViewModel loggedInViewModel;
-//    private ViewManagerModel viewManagerModel;
-//
-//    public LoginPresenter(ViewManagerModel viewManagerModel,
-//                          LoggedInViewModel loggedInViewModel,
-//                          LoginViewModel loginViewModel) {
-//        this.viewManagerModel = viewManagerModel;
-//        this.loggedInViewModel = loggedInViewModel;
-//        this.loginViewModel = loginViewModel;
-//    }
-//
-//    @Override
-//    public void prepareSuccessView(LoginOutputData response) {
-//        // On success, switch to the logged in view.
-//
-//        LoggedInState loggedInState = loggedInViewModel.getState();
-//        loggedInState.setUsername(response.getUsername());
-//        this.loggedInViewModel.setState(loggedInState);
-//        this.loggedInViewModel.firePropertyChanged();
-//
-//        this.viewManagerModel.setActiveView(loggedInViewModel.getViewName());
-//        this.viewManagerModel.firePropertyChanged();
-//    }
-//
-//    @Override
-//    public void prepareFailView(String error) {
-//        LoginState loginState = loginViewModel.getState();
-//        loginState.setUsernameError(error);
-//        loginViewModel.firePropertyChanged();
-//    }
-//}
+package interface_adapter.login;
+
+import interface_adapter.chat.ConversationState;
+import interface_adapter.chat.ConversationViewModel;
+
+import interface_adapter.ViewManagerModel;
+
+import use_case.login.LoginOutputBoundary;
+import use_case.login.LoginOutputData;
+
+
+public class LoginPresenter implements LoginOutputBoundary {
+
+    private final LoginViewModel loginViewModel;
+    private final ConversationViewModel conversationViewModel;
+    private ViewManagerModel viewManagerModel;
+
+    public LoginPresenter(ViewManagerModel viewManagerModel,
+                          ConversationViewModel conversationViewModel,
+                          LoginViewModel loginViewModel) {
+        this.viewManagerModel = viewManagerModel;
+        this.conversationViewModel = conversationViewModel;
+        this.loginViewModel = loginViewModel;
+    }
+
+    @Override
+    public void prepareSuccessView(LoginOutputData response) {
+        // On success, switch to the logged in view.
+
+        ConversationState conversationState = conversationViewModel.getState();
+        conversationState.setConversation(null);  // TODO: Change this to actual conversation
+        this.conversationViewModel.setState(conversationState);
+        this.conversationViewModel.firePropertyChanged();
+
+        this.viewManagerModel.setActiveView(conversationViewModel.getViewName());
+        this.viewManagerModel.firePropertyChanged();
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+        LoginState loginState = loginViewModel.getState();
+        loginState.setUsernameError(error);
+        loginViewModel.firePropertyChanged();
+    }
+}
 
