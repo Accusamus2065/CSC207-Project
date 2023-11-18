df3e0f52a580ff32b73b0549638b8e61428a5e0d
tamlong5 <dexter.tam@mail.utoronto.ca>
2023-11-18 17:47:32 -0500

    Wrote tests for SignupInteractor.validUsername and SignupInteractor.validPassword

--- src/test/java/use_case/signup/SignupInteractorTest.java
+++ src/test/java/use_case/signup/SignupInteractorTest.java
@@ -100,4 +100,99 @@ public class SignupInteractorTest {
         SignupInputData signupInputData = new SignupInputData("taml5", "Abce45", "Abce5", false);
         signupInteractor.execute(signupInputData);
     }
+
+    @Test
+    public void validUsernameTest() {
+        SignupUserDataAccessInterface userDAO = new InMemoryUserDataAccessObject();
+        SignupOutputBoundary signupPresenter = new SignupOutputBoundary() {
+            @Override
+            public void prepareSuccessView(SignupOutputData user) {
+                assertTrue(userDAO.existsByName(false, "taml5"));
+                assertNotNull(user);
+                assertFalse(user.isUseCaseFailed());
+                assertEquals(user.getUsername(), "taml5");
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+                fail("Username expected to pass.");
+            }
+        };
+        DoctorUserFactory docFactory = new DoctorUserFactory();
+        PatientUserFactory patFactory = new PatientUserFactory();
+        SignupInteractor signupInteractor = new SignupInteractor(userDAO, signupPresenter, docFactory, patFactory);
+
+        SignupInputData inputData = new SignupInputData("taml5", "Abc123", "Abc123", false);
+        signupInteractor.execute(inputData);
+    }
+
+    @Test
+    public void invalidUsernameTest() {
+        SignupUserDataAccessInterface userDAO = new InMemoryUserDataAccessObject();
+        SignupOutputBoundary signupPresenter = new SignupOutputBoundary() {
+            @Override
+            public void prepareSuccessView(SignupOutputData user) {
+                fail("Username expected to fail.");
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+                assertEquals("Username is invalid.", error);
+                assertFalse(userDAO.existsByName(false, "a"));
+            }
+        };
+        DoctorUserFactory docFactory = new DoctorUserFactory();
+        PatientUserFactory patFactory = new PatientUserFactory();
+        SignupInteractor signupInteractor = new SignupInteractor(userDAO, signupPresenter, docFactory, patFactory);
+
+        SignupInputData inputData = new SignupInputData("a", "Abc123", "Abc123", false);
+        signupInteractor.execute(inputData);
+    }
+
+    @Test
+    public void validPasswordTest() {
+        SignupUserDataAccessInterface userDAO = new InMemoryUserDataAccessObject();
+        SignupOutputBoundary signupPresenter = new SignupOutputBoundary() {
+            @Override
+            public void prepareSuccessView(SignupOutputData user) {
+                assertTrue(userDAO.existsByName(false, "taml5"));
+                assertNotNull(user);
+                assertEquals(user.getUsername(), "taml5");
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+                fail("Password expected to pass.");
+            }
+        };
+        DoctorUserFactory docFactory = new DoctorUserFactory();
+        PatientUserFactory patFactory = new PatientUserFactory();
+        SignupInteractor signupInteractor = new SignupInteractor(userDAO, signupPresenter, docFactory, patFactory);
+
+        SignupInputData inputData = new SignupInputData("taml5", "Abc123", "Abc123", false);
+        signupInteractor.execute(inputData);
+    }
+
+    @Test
+    public void invalidPasswordTest() {
+        SignupUserDataAccessInterface userDAO = new InMemoryUserDataAccessObject();
+        SignupOutputBoundary signupPresenter = new SignupOutputBoundary() {
+            @Override
+            public void prepareSuccessView(SignupOutputData user) {
+                fail("Password expected to fail.");
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+                assertEquals("Password requires a digit and a letter, be more than 5 characters, and cannot have any other characters.", error);
+                assertFalse(userDAO.existsByName(false, "taml5"));
+            }
+        };
+        DoctorUserFactory docFactory = new DoctorUserFactory();
+        PatientUserFactory patFactory = new PatientUserFactory();
+        SignupInteractor signupInteractor = new SignupInteractor(userDAO, signupPresenter, docFactory, patFactory);
+
+        SignupInputData inputData = new SignupInputData("taml5", "A13", "A13", false);
+        signupInteractor.execute(inputData);
+    }
 }
