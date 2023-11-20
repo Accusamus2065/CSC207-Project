eba0632961facd242f00b01d41ba8c6431e4320c
tamlong5 <dexter.tam@mail.utoronto.ca>
2023-11-20 16:51:01 -0500

    Implemented the SwapToPatientListView use case

---
+++ src/main/java/interface_adapter/swap_views/list_of_patients/SwapToPatientListPresenter.java
@@ -0,0 +1,21 @@
+package interface_adapter.swap_views.list_of_patients;
+// TODO: implement ListOfPatientsViewModel
+//
+//import interface_adapter.ViewManagerModel;
+//import use_case.swap_views.list_of_patients.SwapToPatientListOutputBoundary;
+//
+//public class SwapToPatientListPresenter implements SwapToPatientListOutputBoundary {
+//    public final ViewManagerModel viewManagerModel;
+//    public final ListOfPatientsViewModel listOfPatientsViewModel;
+//
+//    public SwapToPatientListPresenter(ViewManagerModel viewManagerModel, ListOfPatientsViewModel listOfPatientsViewModel) {
+//        this.viewManagerModel = viewManagerModel;
+//        this.listOfPatientsViewModel = listOfPatientsViewModel;
+//    }
+//
+//    @Override
+//    public void swapViews() {
+//        viewManagerModel.setActiveView(listOfPatientsViewModel.getViewName());
+//        viewManagerModel.firePropertyChanged();
+//    }
+//}
