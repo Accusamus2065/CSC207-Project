ba57551c096e0a9497cd98e7bb32ff326ca585bc
Yibin Cui <110690068+Accusamus2065@users.noreply.github.com>
2023-11-22 16:32:50 -0500

    update PatientUpdatePresenter.java and implement prepareSuccessView method

--- src/main/java/interface_adapter/update/patient/PatientUpdatePresenter.java
+++ src/main/java/interface_adapter/update/patient/PatientUpdatePresenter.java
@@ -1,14 +1,36 @@
 package interface_adapter.update.patient;
 
+import interface_adapter.ViewManagerModel;
+import interface_adapter.login.LoginState;
+import interface_adapter.login.LoginViewModel;
 import use_case.update.patient.PatientUpdateOutputBoundary;
 import use_case.update.patient.PatientUpdateOutputData;
 
 public class PatientUpdatePresenter implements PatientUpdateOutputBoundary {
-    //TODO: implement this when logged-in view is done
+    private final LoginViewModel loginViewModel;
+    private final PatientUpdateViewModel patientUpdateViewModel;
+    private final ViewManagerModel viewManagerModel;
+
+    public PatientUpdatePresenter(LoginViewModel loginViewModel,
+                                  PatientUpdateViewModel patientUpdateViewModel,
+                                  ViewManagerModel viewManagerModel) {
+        this.loginViewModel = loginViewModel;
+        this.patientUpdateViewModel = patientUpdateViewModel;
+        this.viewManagerModel = viewManagerModel;
+    }
 
     @Override
-    public void prepareSuccessView(PatientUpdateOutputData user) {
-        assert !user.isUseCaseFailed();
+    public void prepareSuccessView(PatientUpdateOutputData response) {
+        assert !response.isUseCaseFailed();
+        System.out.println("Update successful: username" + response.getUsername());
+
+        LoginState loginState = loginViewModel.getState();
+        loginState.setUsername(response.getUsername());
+        this.loginViewModel.setState(loginState);
+        loginViewModel.firePropertyChanged();
+
+        viewManagerModel.setActiveView(loginViewModel.getViewName());
+        viewManagerModel.firePropertyChanged();
     }
 
     @Override
--- src/main/java/use_case/update/doctor/DoctorUpdateInteractor.java
+++ src/main/java/use_case/update/doctor/DoctorUpdateInteractor.java
@@ -47,4 +47,3 @@ public class DoctorUpdateInteractor implements DoctorUpdateInputBoundary {
         }
     }
 }
-
