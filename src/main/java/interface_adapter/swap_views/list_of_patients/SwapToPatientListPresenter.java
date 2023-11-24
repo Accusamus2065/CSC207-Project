181b008e85d94b7a21b05189f614c11a9fb3dd0f
tamlong5 <dexter.tam@mail.utoronto.ca>
2023-11-24 14:50:49 -0500

    Resolved #45 and connected SwapToPatientList use case to Main

--- src/main/java/interface_adapter/swap_views/list_of_patients/SwapToPatientListPresenter.java
+++ src/main/java/interface_adapter/swap_views/list_of_patients/SwapToPatientListPresenter.java
@@ -1,26 +1,21 @@
 package interface_adapter.swap_views.list_of_patients;
-// TODO: implement ListOfPatientsViewModel
 
 import interface_adapter.ViewManagerModel;
+import interface_adapter.choosepatient.ChoosePatientViewModel;
 import use_case.swap_views.list_of_patients.SwapToPatientListOutputBoundary;
 
 public class SwapToPatientListPresenter implements SwapToPatientListOutputBoundary {
-//    public final ViewManagerModel viewManagerModel;
-//    public final ListOfPatientsViewModel listOfPatientsViewModel;
+    public final ViewManagerModel viewManagerModel;
+    public final ChoosePatientViewModel choosePatientViewModel;
 
-//    public SwapToPatientListPresenter(ViewManagerModel viewManagerModel, ListOfPatientsViewModel listOfPatientsViewModel) {
-//        this.viewManagerModel = viewManagerModel;
-//        this.listOfPatientsViewModel = listOfPatientsViewModel;
-//    }
-
-//    @Override
-//    public void swapViews() {
-//        viewManagerModel.setActiveView(listOfPatientsViewModel.getViewName());
-//        viewManagerModel.firePropertyChanged();
-//    }
+    public SwapToPatientListPresenter(ViewManagerModel viewManagerModel, ChoosePatientViewModel choosePatientViewModel) {
+        this.viewManagerModel = viewManagerModel;
+        this.choosePatientViewModel = choosePatientViewModel;
+    }
 
     @Override
     public void swapViews() {
-        System.out.println("Swapped view to ListOfPatientsView");
+        viewManagerModel.setActiveView(choosePatientViewModel.getViewName());
+        viewManagerModel.firePropertyChanged();
     }
 }
