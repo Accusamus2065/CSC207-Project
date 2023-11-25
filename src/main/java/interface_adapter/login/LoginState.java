ebb2cb4fda85e30de9488f3c16d782a60ca942a1
Yibin Cui <110690068+Accusamus2065@users.noreply.github.com>
2023-11-25 00:15:00 -0500

    update login state to address unused `Error` attributes

--- src/main/java/interface_adapter/login/LoginState.java
+++ src/main/java/interface_adapter/login/LoginState.java
@@ -2,16 +2,14 @@ package interface_adapter.login;
 
 public class LoginState {
     private String username = "";
-    private String usernameError = null;
     private String password = "";
-    private String passwordError = null;
+    private String error = null;
     private boolean isDoctor;
 
     public LoginState(LoginState copy) {
         username = copy.username;
-        usernameError = copy.usernameError;
         password = copy.password;
-        passwordError = copy.passwordError;
+        error = copy.error;
         isDoctor = copy.isDoctor;
     }
 
@@ -22,32 +20,24 @@ public class LoginState {
         return username;
     }
 
-    public String getUsernameError() {
-        return usernameError;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public String getPasswordError() {
-        return passwordError;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setUsernameError(String usernameError) {
-        this.usernameError = usernameError;
-    }
-
     public void setPassword(String password) {
         this.password = password;
     }
 
-    public void setPasswordError(String passwordError) {
-        this.passwordError = passwordError;
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
     }
 
     public boolean isDoctor() {
