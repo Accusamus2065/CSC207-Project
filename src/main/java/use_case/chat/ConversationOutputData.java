81cd9c2aa6d6f21c9b0c0b742a23cf3ace2395d1
Riyad Valiyev <128013175+riyadvaliyev@users.noreply.github.com>
2023-11-27 16:35:33 -0500

    Created and updated some classes

--- src/main/java/use_case/chat/ConversationOutputData.java
+++ src/main/java/use_case/chat/ConversationOutputData.java
@@ -1,17 +1,20 @@
 package use_case.chat;
 
 import entity.chat.Conversation;
+import entity.chat.Message;
 import interface_adapter.chat.ConversationState;
 
+import java.util.List;
+
 public class ConversationOutputData {
-    private Conversation convo;
+    private List<Message> convo;
     private boolean useCaseFailed;
-    public ConversationOutputData(Conversation convo, boolean useCaseFailed) {
+    public ConversationOutputData(List<Message> convo, boolean useCaseFailed) {
         this.convo = convo;
         this.useCaseFailed = useCaseFailed;
     }
 
-    public Conversation getConversation() {
+    public List<Message> getConversation() {
         return convo;
     }
 }
