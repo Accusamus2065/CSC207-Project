17c8a10bd7827f7b0b50a9b8fb6413ff526abb02
Yibin Cui <110690068+Accusamus2065@users.noreply.github.com>
2023-11-30 20:07:43 -0500

    done with SwapToLoginPresenterTest.java

--- src/test/java/interface_adapter/swap_views/login/SwapToLoginPresenterTest.java
+++ src/test/java/interface_adapter/swap_views/login/SwapToLoginPresenterTest.java
@@ -3,6 +3,8 @@ package interface_adapter.swap_views.login;
 import interface_adapter.ViewManagerModel;
 import interface_adapter.choosepatient.ChoosePatientViewModel;
 import interface_adapter.login.LoginViewModel;
+import interface_adapter.update.doctor.DoctorUpdateViewModel;
+import interface_adapter.update.patient.PatientUpdateViewModel;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -14,6 +16,32 @@ public class SwapToLoginPresenterTest {
         ChoosePatientViewModel choosePatientViewModel = new ChoosePatientViewModel();
         LoginViewModel loginViewModel = new LoginViewModel();
         viewManagerModel.setActiveView(choosePatientViewModel.getViewName());
+
+        SwapToLoginPresenter swapToLoginPresenter = new SwapToLoginPresenter(viewManagerModel, loginViewModel);
+        swapToLoginPresenter.execute();
+
+        assertEquals(viewManagerModel.getActiveView(), loginViewModel.getViewName());
+    }
+
+    @Test
+    public void swapPatientUpdateToLoginTest() {
+        ViewManagerModel viewManagerModel = new ViewManagerModel();
+        PatientUpdateViewModel patientupdateViewModel = new PatientUpdateViewModel();
+        LoginViewModel loginViewModel = new LoginViewModel();
+        viewManagerModel.setActiveView(patientupdateViewModel.getViewName());
+
+        SwapToLoginPresenter swapToLoginPresenter = new SwapToLoginPresenter(viewManagerModel, loginViewModel);
+        swapToLoginPresenter.execute();
+
+        assertEquals(viewManagerModel.getActiveView(), loginViewModel.getViewName());
+    }
+
+    @Test
+    public void swapDoctorUpdateToLoginTest() {
+        ViewManagerModel viewManagerModel = new ViewManagerModel();
+        DoctorUpdateViewModel doctorupdateViewModel = new DoctorUpdateViewModel();
+        LoginViewModel loginViewModel = new LoginViewModel();
+        viewManagerModel.setActiveView(doctorupdateViewModel.getViewName());
 
         SwapToLoginPresenter swapToLoginPresenter = new SwapToLoginPresenter(viewManagerModel, loginViewModel);
         swapToLoginPresenter.execute();
