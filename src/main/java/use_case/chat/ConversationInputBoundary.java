f2bec93257141996f31420e66db423b14d03cc9d
Yibin Cui <110690068+Accusamus2065@users.noreply.github.com>
2023-12-01 15:25:02 -0500

    optimize import

--- src/main/java/use_case/chat/ConversationInputBoundary.java
+++ src/main/java/use_case/chat/ConversationInputBoundary.java
@@ -1,9 +1,7 @@
 package use_case.chat;
 
-import view.ConversationView;
-
 public interface ConversationInputBoundary {
     void executeSave(ConversationInputData data);
-    void executeRefresh(ConversationInputData data);
 
+    void executeRefresh(ConversationInputData data);
 }
--- src/main/java/use_case/chat/ConversationInputData.java
+++ src/main/java/use_case/chat/ConversationInputData.java
@@ -4,9 +4,11 @@ import entity.chat.Message;
 
 public class ConversationInputData {
     final private Message message;
-    public ConversationInputData( String sender, String receiver, String messageContent) {
+
+    public ConversationInputData(String sender, String receiver, String messageContent) {
         this.message = new Message(sender, receiver, messageContent, null);
     }
+
     public Message getMessage() {
         return message;
     }
