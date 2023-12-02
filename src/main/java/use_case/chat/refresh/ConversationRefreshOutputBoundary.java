f1385bb18f78d1cce6453216f053c961c9b1b8c1
Riyad Valiyev <128013175+riyadvaliyev@users.noreply.github.com>
2023-12-02 01:18:26 -0500

    Updated ConversationRefreshOutputBoundary.java

--- src/main/java/use_case/chat/refresh/ConversationRefreshOutputBoundary.java
+++ src/main/java/use_case/chat/refresh/ConversationRefreshOutputBoundary.java
@@ -1,7 +1,5 @@
 package use_case.chat.refresh;
 
-import use_case.chat.ConversationOutputData;
-
 public interface ConversationRefreshOutputBoundary {
     void prepareSuccessView(ConversationRefreshOutputData messages);
     void prepareFailView(String error);
