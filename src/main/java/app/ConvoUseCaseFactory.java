98067623ea02f58bccf123f96ac5f38b638ca5d1
Riyad Valiyev <128013175+riyadvaliyev@users.noreply.github.com>
2023-12-02 01:15:00 -0500

    Updated ConvoUseCaseFactory.java

--- src/main/java/app/ConvoUseCaseFactory.java
+++ src/main/java/app/ConvoUseCaseFactory.java
@@ -2,14 +2,17 @@ package app;
 
 import data_access.ConvoDAOImpl;
 import interface_adapter.ViewManagerModel;
-import interface_adapter.chat.ConversationController;
-import interface_adapter.chat.ConversationPresenter;
-import interface_adapter.chat.ConversationViewModel;
+import interface_adapter.chat.refresh.ConversationRefreshController;
+import interface_adapter.chat.refresh.ConversationRefreshPresenter;
+import interface_adapter.chat.refresh.ConversationRefreshViewModel;
+import interface_adapter.chat.save.ConversationSaveController;
 import interface_adapter.login.LoginController;
 import interface_adapter.login.LoginViewModel;
-import use_case.chat.ConversationInputBoundary;
-import use_case.chat.ConversationInteractor;
-import use_case.chat.ConversationOutputBoundary;
+import use_case.chat.refresh.ConversationRefreshInputBoundary;
+import use_case.chat.refresh.ConversationRefreshInteractor;
+import use_case.chat.refresh.ConversationRefreshOutputBoundary;
+import use_case.chat.save.ConversationSaveInputBoundary;
+import use_case.chat.save.ConversationSaveInteractor;
 import use_case.login.LoginUserDataAccessInterface;
 import view.ConversationView;
 import view.LoginView;
@@ -25,12 +28,15 @@ public class ConvoUseCaseFactory {
 
     public static ConversationView create(
             ViewManagerModel viewManagerModel,
-            ConversationViewModel viewModel,
-            ConvoDAOImpl dao, String selfUsername, String otherUsername) {
+            ConversationRefreshViewModel refreshViewModel,
+            ConvoDAOImpl dao, // TODO: why concrete class and not an interface?
+            String selfUsername,
+            String otherUsername) {
 
         try {
-            ConversationController controller = createConversationConroller(viewManagerModel, viewModel, dao);
-            return new ConversationView(viewModel, controller, selfUsername, otherUsername);
+            ConversationRefreshController refreshController = createConversationRefreshConroller(viewManagerModel, refreshViewModel, dao);
+            ConversationSaveController saveController = createConversationSaveController(dao);
+            return new ConversationView(refreshViewModel, refreshController, saveController, selfUsername, otherUsername);
         } catch (IOException e) {
             JOptionPane.showMessageDialog(null, "Could not open user data file.");
         }
@@ -38,11 +44,23 @@ public class ConvoUseCaseFactory {
         return null;
     }
 
-    public static ConversationController createConversationConroller(ViewManagerModel viewManagerModel, ConversationViewModel clearViewModel, ConvoDAOImpl dao) throws IOException {
-        ConversationOutputBoundary outputBoundary = new ConversationPresenter(viewManagerModel, clearViewModel);
-        ConversationInputBoundary inputInteractor = new ConversationInteractor(
-                dao, outputBoundary);
+    private static ConversationRefreshController createConversationRefreshConroller (
+            ViewManagerModel viewManagerModel,
+            ConversationRefreshViewModel refreshViewModel,
+            ConvoDAOImpl dao
+    ) throws IOException {
+        ConversationRefreshOutputBoundary refreshOutputBoundary = new ConversationRefreshPresenter(viewManagerModel,
+                refreshViewModel);
+        ConversationRefreshInputBoundary refreshInteractor = new ConversationRefreshInteractor(
+                dao, refreshOutputBoundary);
 
-        return new ConversationController(inputInteractor);
+        return new ConversationRefreshController(refreshInteractor);
+    }
+
+    private static ConversationSaveController createConversationSaveController (
+            ConvoDAOImpl dao
+    ) throws IOException {
+        ConversationSaveInputBoundary saveInteractor = new ConversationSaveInteractor(dao);
+        return new ConversationSaveController(saveInteractor);
     }
 }
