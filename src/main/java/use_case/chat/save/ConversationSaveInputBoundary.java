2a1098e53e89b2977a859c93b7991c53adcf1675
Riyad Valiyev <128013175+riyadvaliyev@users.noreply.github.com>
2023-12-02 01:18:38 -0500

    Updated ConversationSaveInputBoundary.java

--- src/main/java/use_case/chat/save/ConversationSaveInputBoundary.java
+++ src/main/java/use_case/chat/save/ConversationSaveInputBoundary.java
@@ -1,7 +1,5 @@
 package use_case.chat.save;
 
-import use_case.chat.ConversationInputData;
-
 public interface ConversationSaveInputBoundary {
     void executeSave(ConversationSaveInputData data);
 }
