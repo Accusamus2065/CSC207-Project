d9abccca8f1c9456d142476da02d93a3219e23e3
Riyad Valiyev <128013175+riyadvaliyev@users.noreply.github.com>
2023-12-02 01:14:18 -0500

    Created ConversationRefreshState.java

--- src/main/java/interface_adapter/chat/refresh/ConversationRefreshState.java
+++ src/main/java/interface_adapter/chat/refresh/ConversationRefreshState.java
@@ -1,4 +1,26 @@
 package interface_adapter.chat.refresh;
 
+import entity.chat.Message;
+
+import java.util.List;
+
 public class ConversationRefreshState {
+    private List<Message> messages;
+
+    public ConversationRefreshState(ConversationRefreshState copy) {
+        messages = copy.messages;
+    }
+
+    // Because of the previous copy constructor, the default constructor must be explicit.
+    public ConversationRefreshState() {
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+    }
+
 }
