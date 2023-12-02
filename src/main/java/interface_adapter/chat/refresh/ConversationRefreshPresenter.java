0d4effebc7b6f43c41fd705eba94e32f7752ef9c
Riyad Valiyev <128013175+riyadvaliyev@users.noreply.github.com>
2023-12-02 01:14:09 -0500

    Created ConversationRefreshPresenter.java

--- src/main/java/interface_adapter/chat/refresh/ConversationRefreshPresenter.java
+++ src/main/java/interface_adapter/chat/refresh/ConversationRefreshPresenter.java
@@ -1,4 +1,30 @@
 package interface_adapter.chat.refresh;
 
-public class ConversationRefreshPresenter {
-}
+import interface_adapter.ViewManagerModel;
+import use_case.chat.refresh.ConversationRefreshOutputBoundary;
+import use_case.chat.refresh.ConversationRefreshOutputData;
+
+public class ConversationRefreshPresenter implements ConversationRefreshOutputBoundary {
+    private final ConversationRefreshViewModel conversationRefreshViewModel;
+    private ViewManagerModel viewManagerModel;
+
+    public ConversationRefreshPresenter(ViewManagerModel viewManagerModel, ConversationRefreshViewModel conversationRefreshViewModel) {
+        this.viewManagerModel = viewManagerModel;
+        this.conversationRefreshViewModel = conversationRefreshViewModel;
+    }
+
+    @Override
+    public void prepareSuccessView(ConversationRefreshOutputData messages) {
+        ConversationRefreshState convoRefreshState = conversationRefreshViewModel.getState();
+        convoRefreshState.setMessages(messages.getMessages());
+        this.conversationRefreshViewModel.setState(convoRefreshState);
+        conversationRefreshViewModel.firePropertyChanged();
+        System.out.println(conversationRefreshViewModel.getState().getMessages());
+
+        viewManagerModel.setActiveView(conversationRefreshViewModel.getViewName());
+        viewManagerModel.firePropertyChanged();
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+}}
