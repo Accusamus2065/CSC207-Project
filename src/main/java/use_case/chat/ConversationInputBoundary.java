fe624649334ea9d06d4e75e3bb0053b10f6d7bce
Riyad Valiyev <128013175+riyadvaliyev@users.noreply.github.com>
2023-12-02 01:12:11 -0500

    Deleted ConversationInputBoundary.java as we don't need it anymore. I split that use case into two

--- src/main/java/use_case/chat/ConversationInputBoundary.java
+++
@@ -1,9 +0,0 @@
-package use_case.chat;
-
-import view.ConversationView;
-
-public interface ConversationInputBoundary {
-    void executeSave(ConversationInputData data);
-    void executeRefresh(ConversationInputData data);
-
-}
