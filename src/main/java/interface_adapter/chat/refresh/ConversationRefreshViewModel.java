5c3ea7cdba4fb44e88e1de6026a722a42215a4f4
Riyad Valiyev <128013175+riyadvaliyev@users.noreply.github.com>
2023-12-02 01:14:24 -0500

    Created ConversationRefreshViewModel.java

--- src/main/java/interface_adapter/chat/refresh/ConversationRefreshViewModel.java
+++ src/main/java/interface_adapter/chat/refresh/ConversationRefreshViewModel.java
@@ -1,4 +1,29 @@
 package interface_adapter.chat.refresh;
 
-public class ConversationRefreshViewModel {
+import interface_adapter.ViewModel;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class ConversationRefreshViewModel extends ViewModel {
+
+    public static final String TITLE_LABEL = "Conversation";
+
+    private ConversationRefreshState state = new ConversationRefreshState();
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+    public ConversationRefreshViewModel() {
+        super("conversation view");
+    }
+    public void setState(ConversationRefreshState state) {
+        this.state = state;
+    }
+    public ConversationRefreshState getState() {
+        return state;
+    }
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
 }
