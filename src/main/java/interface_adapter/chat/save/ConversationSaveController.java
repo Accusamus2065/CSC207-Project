3b001e113bdcfd78f32cb902940fe677bb55e3fb
Riyad Valiyev <128013175+riyadvaliyev@users.noreply.github.com>
2023-12-02 01:14:36 -0500

    Created ConversationRefreshController.java

--- src/main/java/interface_adapter/chat/save/ConversationSaveController.java
+++ src/main/java/interface_adapter/chat/save/ConversationSaveController.java
@@ -1,4 +1,14 @@
 package interface_adapter.chat.save;
+import use_case.chat.save.ConversationSaveInputBoundary;
+import use_case.chat.save.ConversationSaveInputData;
 
 public class ConversationSaveController {
+    final ConversationSaveInputBoundary conversationSaveUseCaseInteractor;
+    public ConversationSaveController(ConversationSaveInputBoundary conversationSaveUseCaseInteractor) {
+        this.conversationSaveUseCaseInteractor = conversationSaveUseCaseInteractor;
+    }
+    public void executeSave(String sender, String receiver, String messageContent) {
+        ConversationSaveInputData data = new ConversationSaveInputData(sender, receiver, messageContent);
+        conversationSaveUseCaseInteractor.executeSave(data);
+    }
 }
