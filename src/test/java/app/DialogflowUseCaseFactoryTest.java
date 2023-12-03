7961fb705d4f409bcf9e020c5672574b2b8fde32
Yibin Cui <110690068+Accusamus2065@users.noreply.github.com>
2023-12-03 16:05:23 -0500

    update tests

--- src/test/java/app/DialogflowUseCaseFactoryTest.java
+++ src/test/java/app/DialogflowUseCaseFactoryTest.java
@@ -2,9 +2,11 @@ package app;
 
 import data_access.InMemoryUserDataAccessObject;
 import interface_adapter.ViewManagerModel;
+import interface_adapter.ViewModel;
 import interface_adapter.chat.refresh.ConversationRefreshViewModel;
 import interface_adapter.chatbot.DialogflowViewModel;
 import interface_adapter.login.LoginViewModel;
+import interface_adapter.update.patient.PatientUpdateViewModel;
 import view.DialogflowView;
 import org.junit.Test;
 
@@ -17,12 +19,14 @@ public class DialogflowUseCaseFactoryTest {
         LoginViewModel loginViewModel = new LoginViewModel();
         DialogflowViewModel dialogflowViewModel = new DialogflowViewModel();
         ConversationRefreshViewModel conversationRefreshViewModel = new ConversationRefreshViewModel();
+        PatientUpdateViewModel patientUpdateViewModel = new PatientUpdateViewModel();
         InMemoryUserDataAccessObject dataAccessObject = new InMemoryUserDataAccessObject();
         DialogflowView dialogflowView = DialogflowUseCaseFactory.create(
                 viewManagerModel,
                 loginViewModel,
                 dialogflowViewModel,
                 conversationRefreshViewModel,
+                patientUpdateViewModel,
                 dataAccessObject);
 
         assertEquals(dialogflowViewModel.getViewName(), dialogflowView.viewName);
