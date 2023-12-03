7abf062ac1607a7f344d41d372967bc463b2e081
Riyad Valiyev <128013175+riyadvaliyev@users.noreply.github.com>
2023-12-03 14:30:36 -0500

    Deleted unnecessary uncommented code

--- src/main/java/use_case/chat/refresh/ConversationRefreshInteractor.java
+++ src/main/java/use_case/chat/refresh/ConversationRefreshInteractor.java
@@ -1,30 +1,3 @@
-//package use_case.chat.refresh;
-//
-//import data_access.ConvoDAOImpl;
-//import entity.chat.Message;
-//import use_case.chat.ConversationUserDataAccessInterface;
-//import use_case.chat.save.ConversationSaveInteractor;
-//
-//import java.util.List;
-//
-//public class ConversationRefreshInteractor implements ConversationRefreshInputBoundary {
-//    final ConversationRefreshOutputBoundary presenter;
-//    final ConversationUserDataAccessInterface dao;
-//
-//    public ConversationRefreshInteractor(ConversationUserDataAccessInterface convDao,
-//                                         ConversationRefreshOutputBoundary presenter) {
-//        this.dao = convDao;
-//        this.presenter = presenter;
-//    }
-//    @Override
-//    public void executeRefresh(ConversationRefreshInputData data) {
-//        List<Message> messages = dao.query(data.getMessage().getSender(), data.getMessage().getReceiver());
-//        ConversationRefreshOutputData outputData = new ConversationRefreshOutputData(messages);
-//        presenter.prepareSuccessView(outputData);
-//    }
-//
-//}
-
 package use_case.chat.refresh;
 
 import entity.chat.Message;
