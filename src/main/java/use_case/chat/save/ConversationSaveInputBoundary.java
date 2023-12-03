5f1a1273c09f5f5b264946e7e8a8ebc2c3b1827f
Riyad Valiyev <128013175+riyadvaliyev@users.noreply.github.com>
2023-12-02 23:19:58 -0500

    Provided descriptive comments for the codes in this file

--- src/main/java/use_case/chat/save/ConversationSaveInputBoundary.java
+++ src/main/java/use_case/chat/save/ConversationSaveInputBoundary.java
@@ -1,5 +1,20 @@
+//package use_case.chat.save;
+//
+//public interface ConversationSaveInputBoundary {
+//    void executeSave(ConversationSaveInputData data);
+//}
 package use_case.chat.save;
 
+/**
+ * The ConversationSaveInputBoundary interface defines the input boundary for the conversation save use case.
+ * It declares the method signature for executing the save operation based on the provided input data.
+ */
 public interface ConversationSaveInputBoundary {
+
+    /**
+     * Executes the save operation for a conversation based on the provided input data.
+     *
+     * @param data The input data containing information necessary for the save operation.
+     */
     void executeSave(ConversationSaveInputData data);
 }
