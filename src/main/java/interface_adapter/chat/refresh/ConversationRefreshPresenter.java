d96616948e9443d7a96b378b204290830e816377
Riyad Valiyev <128013175+riyadvaliyev@users.noreply.github.com>
2023-12-03 14:30:44 -0500

    Deleted unnecessary uncommented code

--- src/main/java/interface_adapter/chat/refresh/ConversationRefreshPresenter.java
+++ src/main/java/interface_adapter/chat/refresh/ConversationRefreshPresenter.java
@@ -1,35 +1,3 @@
-//package interface_adapter.chat.refresh;
-//
-//import interface_adapter.ViewManagerModel;
-//import use_case.chat.refresh.ConversationRefreshOutputBoundary;
-//import use_case.chat.refresh.ConversationRefreshOutputData;
-//
-//public class ConversationRefreshPresenter implements ConversationRefreshOutputBoundary {
-//    private final ConversationRefreshViewModel conversationRefreshViewModel;
-//    private ViewManagerModel viewManagerModel;
-//
-//    public ConversationRefreshPresenter(ViewManagerModel viewManagerModel, ConversationRefreshViewModel conversationRefreshViewModel) {
-//        this.viewManagerModel = viewManagerModel;
-//        this.conversationRefreshViewModel = conversationRefreshViewModel;
-//    }
-//
-//    @Override
-//    public void prepareSuccessView(ConversationRefreshOutputData messages) {
-//        ConversationRefreshState convoRefreshState = conversationRefreshViewModel.getState();
-//        convoRefreshState.setMessages(messages.getMessages());
-//        this.conversationRefreshViewModel.setState(convoRefreshState);
-//        conversationRefreshViewModel.firePropertyChanged();
-//        System.out.println(conversationRefreshViewModel.getState().getMessages());
-//
-//        viewManagerModel.setActiveView(conversationRefreshViewModel.getViewName());
-//        viewManagerModel.firePropertyChanged();
-//    }
-//
-//    @Override
-//    public void prepareFailView(String error) {
-//    }
-//}
-
 package interface_adapter.chat.refresh;
 
 import interface_adapter.ViewManagerModel;
