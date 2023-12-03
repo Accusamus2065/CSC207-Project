aadcf2022562f65fd8bcffaf478384df8e4cbdfd
Riyad Valiyev <128013175+riyadvaliyev@users.noreply.github.com>
2023-12-02 23:20:03 -0500

    Provided descriptive comments for the codes in this file

--- src/main/java/use_case/chat/save/ConversationSaveInteractor.java
+++ src/main/java/use_case/chat/save/ConversationSaveInteractor.java
@@ -1,16 +1,50 @@
-package use_case.chat.save;
+//package use_case.chat.save;
+//
+//import data_access.ConvoDAOImpl;
+//import use_case.chat.ConversationUserDataAccessInterface;
+//
+//public class ConversationSaveInteractor implements ConversationSaveInputBoundary{
+//    final ConversationUserDataAccessInterface dao;
+//
+//    public ConversationSaveInteractor(ConversationUserDataAccessInterface convDao){
+//       this.dao = convDao;
+//    }
+//    @Override
+//    public void executeSave(ConversationSaveInputData data) {
+//        dao.save(data.getMessage());    // no output TODO: no output for save message
+//    }
+//}
 
-import data_access.ConvoDAOImpl;
+package use_case.chat.save;
 import use_case.chat.ConversationUserDataAccessInterface;
 
-public class ConversationSaveInteractor implements ConversationSaveInputBoundary{
+/**
+ * The ConversationSaveInteractor class represents the interactor for the conversation save use case.
+ * It interacts with the data access layer to execute the save operation for a conversation message.
+ */
+public class ConversationSaveInteractor implements ConversationSaveInputBoundary {
+
+    // The data access interface for interacting with the conversation data
     final ConversationUserDataAccessInterface dao;
 
-    public ConversationSaveInteractor(ConversationUserDataAccessInterface convDao){
-       this.dao = convDao;
+    /**
+     * Constructor for ConversationSaveInteractor.
+     *
+     * @param convDao The conversation data access interface.
+     */
+    public ConversationSaveInteractor(ConversationUserDataAccessInterface convDao) {
+        this.dao = convDao;
     }
+
+    /**
+     * Executes the save operation for a conversation based on the provided input data.
+     *
+     * @param data The input data containing information necessary for the save operation.
+     */
     @Override
     public void executeSave(ConversationSaveInputData data) {
-        dao.save(data.getMessage());    // no output TODO: no output for save message
+        // Save the conversation message through the data access interface
+        dao.save(data.getMessage());
+        // No output for save operation, hence no return value or output data
     }
 }
