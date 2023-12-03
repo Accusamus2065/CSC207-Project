4c529e30134ca19aab9041f66ca9ccdb431b5b44
Riyad Valiyev <128013175+riyadvaliyev@users.noreply.github.com>
2023-12-02 23:19:42 -0500

    Provided descriptive comments for the codes in this file

--- src/main/java/use_case/chat/refresh/ConversationRefreshOutputBoundary.java
+++ src/main/java/use_case/chat/refresh/ConversationRefreshOutputBoundary.java
@@ -1,6 +1,29 @@
+//package use_case.chat.refresh;
+//
+//public interface ConversationRefreshOutputBoundary {
+//    void prepareSuccessView(ConversationRefreshOutputData messages);
+//    void prepareFailView(String error);
+//}
+
 package use_case.chat.refresh;
 
+/**
+ * The ConversationRefreshOutputBoundary interface defines the output boundary for the conversation refresh use case.
+ * It declares methods for preparing views in case of a successful refresh and in case of a failure.
+ */
 public interface ConversationRefreshOutputBoundary {
+
+    /**
+     * Prepares the view for a successful conversation refresh by providing the output data.
+     *
+     * @param messages The output data containing refreshed conversation messages.
+     */
     void prepareSuccessView(ConversationRefreshOutputData messages);
+
+    /**
+     * Prepares the view for a failed conversation refresh by providing an error message.
+     *
+     * @param error The error message indicating the cause of the failure.
+     */
     void prepareFailView(String error);
 }
