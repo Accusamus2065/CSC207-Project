d86fbc490353e30b9b3d1f813d77b53fdd706823
tamlong5 <dexter.tam@mail.utoronto.ca>
2023-12-02 21:13:36 -0500

    Wrote tests for ConversationRefreshInteractor and ConversationSaveInteractor

---
+++ src/test/java/use_case/chat/refresh/ConversationRefreshInteractorTest.java
@@ -0,0 +1,44 @@
+package use_case.chat.refresh;
+
+import data_access.InMemoryConversationDataAccessObject;
+import entity.chat.Message;
+import org.junit.Test;
+import use_case.chat.ConversationUserDataAccessInterface;
+
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+public class ConversationRefreshInteractorTest {
+    private static final String SENDER = "TestSender";
+    private static final String RECEIVER = "TestReceiver";
+    private static final String CONTENT = "TestMessage";
+    private static final Date DATE = new Date();
+    @Test
+    public void refreshMessageTest() {
+        ConversationUserDataAccessInterface dataAccessObject = new InMemoryConversationDataAccessObject();
+        Message message = new Message(SENDER, RECEIVER, CONTENT, DATE);
+        dataAccessObject.save(message);
+        ConversationRefreshOutputBoundary presenter = new ConversationRefreshOutputBoundary() {
+            @Override
+            public void prepareSuccessView(ConversationRefreshOutputData messages) {
+                assertEquals(1, messages.getMessages().size());
+                Message message = messages.getMessages().get(0);
+
+                assertEquals(SENDER, message.getSender());
+                assertEquals(RECEIVER, message.getReceiver());
+                assertEquals(CONTENT, message.getContent());
+                assertEquals(DATE, message.getTimestamp());
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+                fail("Test not expected to fail.");
+            }
+        };
+        ConversationRefreshInputData inputData = new ConversationRefreshInputData(SENDER, RECEIVER);
+
+        ConversationRefreshInteractor interactor = new ConversationRefreshInteractor(dataAccessObject, presenter);
+        interactor.executeRefresh(inputData);
+    }
+}
