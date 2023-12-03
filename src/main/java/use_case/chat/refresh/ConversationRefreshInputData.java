3c3272786c1b69f9cccff869f148274fd2e741a8
Riyad Valiyev <128013175+riyadvaliyev@users.noreply.github.com>
2023-12-02 23:19:38 -0500

    Provided descriptive comments for the codes in this file

--- src/main/java/use_case/chat/refresh/ConversationRefreshInputData.java
+++ src/main/java/use_case/chat/refresh/ConversationRefreshInputData.java
@@ -1,17 +1,52 @@
+//package use_case.chat.refresh;
+//
+//import entity.chat.Message;
+//
+//import javax.xml.crypto.Data;
+//import java.security.Timestamp;
+//import java.time.LocalDateTime;
+//import java.util.Date;
+//
+//public class ConversationRefreshInputData {
+//    final private Message message;
+//    public ConversationRefreshInputData(String sender, String receiver) {
+//        this.message = new Message(sender, receiver, null, null);
+//    }
+//    public Message getMessage() {
+//        return message;
+//    }
+//}
+
 package use_case.chat.refresh;
 
 import entity.chat.Message;
 
-import javax.xml.crypto.Data;
-import java.security.Timestamp;
-import java.time.LocalDateTime;
-import java.util.Date;
-
+/**
+ * The ConversationRefreshInputData class represents the input data for the conversation refresh use case.
+ * It encapsulates information such as the sender, receiver, and other details needed for the refresh operation.
+ */
 public class ConversationRefreshInputData {
+
+    // The message object containing sender, receiver, and other details
     final private Message message;
+
+    /**
+     * Constructor for ConversationRefreshInputData.
+     * Initializes the input data with the sender, receiver, and other details.
+     *
+     * @param sender   The sender of the conversation.
+     * @param receiver The receiver of the conversation.
+     */
     public ConversationRefreshInputData(String sender, String receiver) {
+        // Create a new Message object with sender, receiver, and null values for content and timestamp
         this.message = new Message(sender, receiver, null, null);
     }
+
+    /**
+     * Getter for retrieving the Message object encapsulated in the input data.
+     *
+     * @return The Message object.
+     */
     public Message getMessage() {
         return message;
     }
